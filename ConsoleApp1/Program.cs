ec3eba81d40d022b15fc9931d160ee4dd749b0ab
Dorota Kopczyk <4575341+dkopczyk@users.noreply.github.com>
2019-01-20 16:02:04 -0600

    Set of work is done per chromosome, and then verify positions.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,26 +14,16 @@ namespace ConsoleApp1
 
             try
             {
-                List<Marker> filedata = new List<Marker>();
-                
-                foreach (var line in File.ReadLines("C:\\Users\\Dorota Kopczyk\\Downloads\\input.txt").Skip(1))
+                var dataset = File.ReadLines("C:\\Users\\Dorota Kopczyk\\Downloads\\input.txt").Skip(1);
+                var markers =  TransformInputFileToListOfObjects(dataset);
+                var chromosomeSets = markers.GroupBy(c => c.Chromosome).Select(c => c.ToList()).ToList();
+
+                // All the work is done for a set of chromosomes,
+                // where first the chromosomes are in order and then so are positions. (genomic order)
+                foreach (var chromosomeSet in chromosomeSets)
                 {
-                    var record = new Marker();
-                    var tempLine = line.Split('\t');
-                    record.Name = tempLine[0];
-                    record.Chromosome = tempLine[1];
-                    record.Position = Int32.Parse(tempLine[2]);
-                    
-                    try
-                    {
-                        record.Pvalue = Double.Parse(tempLine[3]);
-                        filedata.Add(record);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Could not parse pvalue for " + tempLine[3]);
-                    }
-                    
+                    //Verify position
+                    var isSorted = IsSorted(chromosomeSet);
                 }
             }
             catch (Exception e)
@@ -44,6 +34,50 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("Executing finally block.");
             }
+        }
+
+        private static List<Marker> TransformInputFileToListOfObjects(IEnumerable<string> dataset)
+        {
+            List<Marker> filedata = new List<Marker>();
+
+            foreach (var line in dataset)
+            {
+                var record = new Marker();
+                var tempLine = line.Split('\t');
+                record.Name = tempLine[0];
+                record.Chromosome = tempLine[1];
+                record.Position = Int32.Parse(tempLine[2]);
+
+                try
+                {
+                    record.Pvalue = Double.Parse(tempLine[3]);
+                    filedata.Add(record);
+                }
+                catch (Exception e)
+                {
+                    // Assuming we can skip any line with an unavailable/ unparsable pvalue
+                    Console.WriteLine("Could not parse pvalue for " + tempLine[3]);
+                }
+            }
+
+            return filedata;
+        }
+
+        /// <summary>
+        /// Determines if int list (this can be an array too) is sorted from 0 -> Max
+        /// </summary>
+        public static bool IsSorted(List<Marker> listMarkers)
+        {
+            for (int i = 1; i < listMarkers.Count; i++)
+            {
+                var previousPosition = listMarkers[i - 1].Position;
+                var currentPosition = listMarkers[i].Position;
+                if (previousPosition > currentPosition)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public double ParsePValue(string value)
